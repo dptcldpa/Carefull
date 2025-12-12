4fa70256a82c7b64c9f54a698584bbf609522385
PEANUTBUTTER1001 <yangjk9203@naver.com>
2025-12-12 21:58:48 +0900

    refactor: 식단 repository의 flow 패턴 표준화

--- data/src/main/java/com/cases/carefull/data/repository/routine/diet/FavoriteFoodRepositoryImpl.kt
+++ data/src/main/java/com/cases/carefull/data/repository/routine/diet/FavoriteFoodRepositoryImpl.kt
@@ -5,25 +5,46 @@ import com.cases.carefull.data.mapper.toDomain
 import com.cases.carefull.data.mapper.toEntity
 import com.cases.carefull.domain.model.routine.diet.FavoriteFood
 import com.cases.carefull.domain.repository.routine.diet.FavoriteFoodRepository
+import com.cases.carefull.domain.util.DataResourceResult
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class FavoriteFoodRepositoryImpl @Inject constructor(
     private val favoriteFoodDao: FavoriteFoodDao
 ) : FavoriteFoodRepository {
-    override fun getAllFavoriteFoods(): Flow<List<FavoriteFood>> {
+
+    override fun getAllFavoriteFoods(): Flow<DataResourceResult<List<FavoriteFood>>> {
         return favoriteFoodDao.getAllFavoriteFoods()
             .map { entities ->
-                entities.map { it.toDomain() }
+                val domainModel = entities.map { it.toDomain() }
+                DataResourceResult.Success(domainModel) as DataResourceResult<List<FavoriteFood>>
             }
+            .onStart { emit(DataResourceResult.Loading) }
+            .catch { emit(DataResourceResult.Error(it)) }
+            .flowOn(Dispatchers.IO)
     }
 
-    override suspend fun saveFavoriteFood(favoriteFood: FavoriteFood) {
-        favoriteFoodDao.insertFavoriteFood(favoriteFood.toEntity())
-    }
+    override fun saveFavoriteFood(favoriteFood: FavoriteFood): Flow<DataResourceResult<Boolean>> =
+        flow {
+            emit(DataResourceResult.Loading)
+            favoriteFoodDao.insertFavoriteFood(favoriteFood.toEntity())
+            emit(DataResourceResult.Success(true))
+        }
+            .catch { emit(DataResourceResult.Error(it)) }
+            .flowOn(Dispatchers.IO)
 
-    override suspend fun deleteFavoriteFood(favoriteFood: FavoriteFood) {
-        favoriteFoodDao.deleteFavoriteFood(favoriteFood.toEntity())
-    }
+    override fun deleteFavoriteFood(favoriteFood: FavoriteFood): Flow<DataResourceResult<Boolean>> =
+        flow {
+            emit(DataResourceResult.Loading)
+            favoriteFoodDao.deleteFavoriteFood(favoriteFood.toEntity())
+            emit(DataResourceResult.Success(true))
+        }
+            .catch { emit(DataResourceResult.Error(it)) }
+            .flowOn(Dispatchers.IO)
 }
--- domain/src/main/java/com/cases/carefull/domain/repository/routine/diet/FavoriteFoodRepository.kt
+++ domain/src/main/java/com/cases/carefull/domain/repository/routine/diet/FavoriteFoodRepository.kt
@@ -1,10 +1,11 @@
 package com.cases.carefull.domain.repository.routine.diet
 
 import com.cases.carefull.domain.model.routine.diet.FavoriteFood
+import com.cases.carefull.domain.util.DataResourceResult
 import kotlinx.coroutines.flow.Flow
 
 interface FavoriteFoodRepository {
-    fun getAllFavoriteFoods(): Flow<List<FavoriteFood>>
-    suspend fun saveFavoriteFood(favoriteFood: FavoriteFood)
-    suspend fun deleteFavoriteFood(favoriteFood: FavoriteFood)
+    fun getAllFavoriteFoods(): Flow<DataResourceResult<List<FavoriteFood>>>
+    fun saveFavoriteFood(favoriteFood: FavoriteFood): Flow<DataResourceResult<Boolean>>
+    fun deleteFavoriteFood(favoriteFood: FavoriteFood): Flow<DataResourceResult<Boolean>>
 }
