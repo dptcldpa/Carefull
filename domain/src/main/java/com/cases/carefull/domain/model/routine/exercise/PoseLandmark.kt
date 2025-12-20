cbfff48780b94486f47a1d730453cb1f16b1faaf
PEANUTBUTTER1001 <yangjk9203@naver.com>
2025-12-20 22:25:42 +0900

    refactor: PoseLandmark 인덱스 상수화

---
+++ domain/src/main/java/com/cases/carefull/domain/model/routine/exercise/PoseLandmark.kt
@@ -0,0 +1,37 @@
+package com.cases.carefull.domain.model.routine.exercise
+
+object PoseLandmark {
+    val NOSE: Int = 0
+    val LEFT_EYE_INNER: Int = 1
+    val LEFT_EYE: Int = 2
+    val LEFT_EYE_OUTER: Int = 3
+    val RIGHT_EYE_INNER: Int = 4
+    val RIGHT_EYE: Int = 5
+    val RIGHT_EYE_OUTER: Int = 6
+    val LEFT_EAR: Int = 7
+    val RIGHT_EAR: Int = 8
+    val LEFT_MOUTH: Int = 9
+    val RIGHT_MOUTH: Int = 10
+    val LEFT_SHOULDER: Int = 11
+    val RIGHT_SHOULDER: Int = 12
+    val LEFT_ELBOW: Int = 13
+    val RIGHT_ELBOW: Int = 14
+    val LEFT_WRIST: Int = 15
+    val RIGHT_WRIST: Int = 16
+    val LEFT_PINKY: Int = 17
+    val RIGHT_PINKY: Int = 18
+    val LEFT_INDEX: Int = 19
+    val RIGHT_INDEX: Int = 20
+    val LEFT_THUMB: Int = 21
+    val RIGHT_THUMB: Int = 22
+    val LEFT_HIP: Int = 23
+    val RIGHT_HIP: Int = 24
+    val LEFT_KNEE: Int = 25
+    val RIGHT_KNEE: Int = 26
+    val LEFT_ANKLE: Int = 27
+    val RIGHT_ANKLE: Int = 28
+    val LEFT_HEEL: Int = 29
+    val RIGHT_HEEL: Int = 30
+    val LEFT_FOOT_INDEX: Int = 31
+    val RIGHT_FOOT_INDEX: Int = 32
+}
